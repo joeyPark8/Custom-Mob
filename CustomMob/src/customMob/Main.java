1d343cba3e61d5f810d963bdfa3c825ecefe32b3
joeyPark8 <61180930+joeyPark8@users.noreply.github.com>
2020-12-22 14:48:56 +0900

    Add files via upload

--- CustomMob/src/customMob/Main.java
+++ CustomMob/src/customMob/Main.java
@@ -1,5 +1,6 @@
 package customMob;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.*;
@@ -19,11 +20,27 @@ public class Main extends JavaPlugin {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
-        if (command.getName().equalsIgnoreCase("za")) {
-            Horse horse = (Horse) player.getWorld().spawnEntity(player.getLocation(), EntityType.HORSE);
-            Pillager pillager = (Pillager) player.getWorld().spawnEntity(player.getLocation(), EntityType.PILLAGER);
-            horse.setPassenger(pillager);
-            return false;
+        if (command.getName().equalsIgnoreCase("/monster")) {
+            if (args.length == 0) {
+                sender.sendMessage(ChatColor.GOLD + "cookie monster!!!");
+            }
+            else if (args[0].equalsIgnoreCase("ph")) {
+                Horse horse = (Horse) player.getWorld().spawnEntity(player.getLocation(), EntityType.HORSE);
+                Pillager pillager = (Pillager) player.getWorld().spawnEntity(player.getLocation(), EntityType.PILLAGER);
+                horse.setPassenger(pillager);
+                return false;
+            }
+            else if (args[0].equalsIgnoreCase("ss")) {
+                SkeletonHorse skeletonHorse = (SkeletonHorse) player.getWorld().spawnEntity(player.getLocation(), EntityType.SKELETON_HORSE);
+                Skeleton skeleton = (Skeleton) player.getWorld().spawnEntity(player.getLocation(), EntityType.SKELETON);
+                skeletonHorse.setPassenger(skeleton);
+                return false;
+            }
+            else if (args[0].equalsIgnoreCase("zz")) {
+                ZombieHorse zombieHorse = (ZombieHorse) player.getWorld().spawnEntity(player.getLocation(), EntityType.ZOMBIE_HORSE);
+                Zombie zombie = (Zombie) player.getWorld().spawnEntity(player.getLocation(), EntityType.ZOMBIE);
+                zombieHorse.setPassenger(zombie);
+            }
         }
         return true;
     }
